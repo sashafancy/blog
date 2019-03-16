260226b067e628980e553688af4f9f5c72647a73
Sophie Zhang <alohalovesweet2014@gmail.com>
2019-03-15 21:48:07 -0700

    Thymeleaf demo

--- blog/blog/src/main/java/com/example/blog/HelloWorldController.java
+++ blog/blog/src/main/java/com/example/blog/HelloWorldController.java
@@ -1,14 +1,18 @@
 package com.example.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
-    public String greeting () {
-        return "Hello world!";
+    public String greeting (Model theModel) {
+
+        theModel.addAttribute("theDate", new java.util.Date());
+        return "helloworld";
     }
 }
