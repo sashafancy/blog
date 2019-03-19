0f51bf858ee84a3911b95031d09d984d73cc111e
Sophie Zhang <alohalovesweet2014@gmail.com>
2019-03-18 22:41:21 -0700

    TestSuite

---
+++ src/test/java/com/example/blog/TestSuite.java
@@ -0,0 +1,10 @@
+package com.example.blog;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.example.blog")
+public class TestSuite {
+}
