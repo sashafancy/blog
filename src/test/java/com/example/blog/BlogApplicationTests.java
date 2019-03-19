ea7caec9d6865d442310331f46d95bc38053fa73
Sophie Zhang <alohalovesweet2014@gmail.com>
2019-03-18 21:51:46 -0700

    Clean up

--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -3,20 +3,10 @@ package com.example.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 @SpringBootTest
 public class BlogApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		assertThrows(IOException.class, () -> t());
-	}
-
-	private static void t() throws IOException {
-		throw new IOException();
-	}
+	public void contextLoads() {}
 
 }
