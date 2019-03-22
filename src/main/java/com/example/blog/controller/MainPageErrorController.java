f0e89fc1fd0f94c575b5550d0549670f2165d92e
Sophie Zhang <alohalovesweet2014@gmail.com>
2019-03-21 21:09:21 -0700

    Modify error page

--- src/main/java/com/example/blog/controller/MainPageErrorController.java
+++ src/main/java/com/example/blog/controller/MainPageErrorController.java
@@ -2,7 +2,9 @@ package com.example.blog.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,18 +12,24 @@ import javax.servlet.http.HttpServletRequest;
 public class MainPageErrorController implements ErrorController {
 
   @RequestMapping("/error")
-  public String handleError(HttpServletRequest request) {
+  public String handleError(HttpServletRequest request, Model theModel) {
 
-    Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-    if (statusCode == 401) {
-      return "/401";
-    } else if (statusCode == 404){
-      return "/404";
-    } else if (statusCode == 403) {
-      return "/403";
+    int statusCode = (int) request.getAttribute("javax.servlet.error.status_code");
+    String errorMsg = "";
+
+    if (statusCode == 400) {
+      errorMsg = "Bad Request";
+    } else if (statusCode == 401){
+      errorMsg = "Unauthorized";
+    } else if (statusCode == 404) {
+      errorMsg = "Resource not found";
     } else {
-      return "/500";
+      errorMsg = "Http Error Code: 500. Internal Server Error";
     }
+
+    theModel.addAttribute("errorCode", statusCode);
+    theModel.addAttribute("errorMsg", errorMsg);
+    return "error";
   }
 
   @Override
